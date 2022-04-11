267805dc41f958ddcde43feeed2dabd65900b2aa
unknown <Shanereeve@gmail.com>
2022-04-10 20:25:33 -0600

    Resetting Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,6 +52,8 @@ export class AppComponent {
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
     this.submitted = true;
+
+    this.signupForm.reset();
   }
 }
 
